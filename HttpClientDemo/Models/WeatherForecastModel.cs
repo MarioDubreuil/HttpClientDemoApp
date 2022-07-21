c05accec3e3ff8c0b0046086b1a2ecc2e134c957
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-07-20 23:04:39 -0400

    Implement weather page

--- HttpClientDemo/Models/WeatherForecastModel.cs
+++ HttpClientDemo/Models/WeatherForecastModel.cs
@@ -1,50 +1,23 @@
-﻿namespace HttpClientDemo.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace HttpClientDemo.Models;
 
 public class WeatherForecastModel
 {
-    public object[] context { get; set; }
-    public string type { get; set; }
-    public GeometryModel geometry { get; set; }
-    public PropertiesModel properties { get; set; }
+    public string countryabbreviation { get; set; }
+    public Place[] places { get; set; }
+    public string country { get; set; }
+    public string placename { get; set; }
+    public string state { get; set; }
+    public string stateabbreviation { get; set; }
 }
 
-public class GeometryModel
+public class Place
 {
-    public string type { get; set; }
-    public float[][][] coordinates { get; set; }
-}
-
-public class PropertiesModel
-{
-    public DateTime updated { get; set; }
-    public string units { get; set; }
-    public string forecastGenerator { get; set; }
-    public DateTime generatedAt { get; set; }
-    public DateTime updateTime { get; set; }
-    public string validTimes { get; set; }
-    public ElevationModel elevation { get; set; }
-    public PeriodModel[] periods { get; set; }
-}
-
-public class ElevationModel
-{
-    public string unitCode { get; set; }
-    public float value { get; set; }
-}
-
-public class PeriodModel
-{
-    public int number { get; set; }
-    public string name { get; set; }
-    public DateTime startTime { get; set; }
-    public DateTime endTime { get; set; }
-    public bool isDaytime { get; set; }
-    public int temperature { get; set; }
-    public string temperatureUnit { get; set; }
-    public object temperatureTrend { get; set; }
-    public string windSpeed { get; set; }
-    public string windDirection { get; set; }
-    public string icon { get; set; }
-    public string shortForecast { get; set; }
-    public string detailedForecast { get; set; }
+    [JsonPropertyName("place name")]
+    public string placename { get; set; }
+    public string longitude { get; set; }
+    [JsonPropertyName("post code")]
+    public string postcode { get; set; }
+    public string latitude { get; set; }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # HttpClientDemoApp
 
 https://api.weather.gov/gridpoints/TOP/31,80/forecast
+https://api.zippopotam.us/us/ma/boston
